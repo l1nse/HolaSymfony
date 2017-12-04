288c17a5a703e7bd27835987f8a3c45f4e08848e
l1nse <gustavoignacio01@gmail.com>
2017-12-04 01:12:27 -0300

    metodo GET

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,6 +4,9 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
@@ -20,14 +23,16 @@ class DefaultController extends Controller
     }
 
     /**
-     * @Route("/", name="homepage")
+     * @Route("/producto", name="producto_list")
+     * @Method({"GET"})     
      */
-    public function indexAction(Request $request)
+    public function listProducto(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
-        ]);
+        return new Response(
+                '<html><body>'.'Nro de producto : '.$request->get('nro').'</body></html>'   
+        );
+    
     }
 }
-}
+
